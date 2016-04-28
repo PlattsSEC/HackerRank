6b524c7a5e65d8a8bb26b1e05c6363630590cf1a
Prithaj Nath <pnath001@cslab-156.cs.plattsburgh.edu>
2016-04-28 16:40:49 -0400

    removed spaces

--- monotranscipher.py
+++ monotranscipher.py
@@ -4,63 +4,39 @@ letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def rem_dep(astring):
 
      my_set = set(astring)
-
      new_string = ""
-
      for i in astring:
-
           if i in my_set:
                new_string = new_string + i
                my_set = my_set - set(i)
-
      return new_string
 
 def cipherkey(key):
 
-    #someshit
+
     my_key = rem_dep(key)
-
     my_array = []
-
     my_array.append(list(my_key))
-
     my_len = len(my_key)
-
-    i = 0
-
-    #print(list(repetitions("SECRET")))
-
-
-    #make new_letters here
-
     my_letters = letters
 
     for j in range(my_len):
 
         my_letters = my_letters.translate(None,my_key[j])
-        #print("This is j :"+str(j))
-
-    #print(letters)
-
 
     val = 0
 
     if ((26-my_len)%my_len==0):
          my_range = (26-my_len)/my_len
          for p in range(my_range):
-              #something
               my_array.append(list(my_letters[val:val+my_len]))
               val = my_len + val
     else:
          my_range = ((26-my_len)/my_len)+1
          for p in range(my_range):
               
-              #something
               my_array.append(list(my_letters[val:val+my_len]))
               val = my_len + val
-
-
-    #print(my_array)
 
     my_column = []
     my_string = ""
@@ -77,20 +53,11 @@ def cipherkey(key):
         my_column.append(my_string)
         my_string = ""
 
-    '''
-    for k in my_array:
-         if k:
-              #something
-              my_column.append("".join(k))
-    #print("This is my_array: "+str(my_array))
-    print("This is my_column: "+ str(my_column))
-    '''
     sort_string = ""
 
     for x in my_column:
         sort_string = sort_string + x[0]
     quickref = sorted(list(sort_string))
-    #print(quickref)
     new_column = []
     for y in quickref:
 
@@ -102,22 +69,14 @@ def cipherkey(key):
             else:
                 continue
 
-
-
-    #print("This is new_column: "+ str(new_column))
-
     comp_string = "".join(new_column)
-    #print("This is comp_string: "+ str(comp_string)+ str(len(comp_string)))
     return comp_string
 
 def uncipher(key,text):
 
     my_string = ""
     letter_list = list(letters)
-
     ref = list(cipherkey(key))
-
-    #print("This is ref"+str(ref)+" "+str(len(ref)))
 
     for i in text:
         my_string = my_string + letter_list[ref.index(i)]
