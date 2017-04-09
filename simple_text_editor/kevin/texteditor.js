fb9ea37749bc7e7b7181a5d5dd9afc9c8432a820
Kevin Boyette <kevin.boyette042@gmail.com>
2017-04-08 21:10:21 -0400

    Switch to Jasmine from Tape
    *  Add Tests for texteditor.js and pair.js

--- simple_text_editor/kevin/texteditor.js
+++ simple_text_editor/kevin/texteditor.js
@@ -4,16 +4,15 @@ const Pair = require('./pair')
 
 class TextEditor {
   constructor () {
-    this.input_stack = []
+    this.inputStack = []
     this.string = ''
   }
   createOperation (op, word) {
     return new Pair(op, word)
   }
   append (input) {
-    console.log(`appending ${input}`)
     if (typeof input === 'string' || input instanceof String) {
-      this.input_stack.push(this.create_operation(1, input))
+      this.inputStack.push(this.createOperation(1, input))
       this.string += input
     } else {
       console.log(`Invalid input type. Type of ${input} is not of type string...`)
@@ -22,11 +21,10 @@ class TextEditor {
   }
 
   delete (k) {
-    console.log(`deleting ${k}`)
     if (Number.isInteger(k)) {
       const suffix = this.string.slice(-k)
       this.string = this.string.substr(0, this.string.length - k)
-      this.input_stack.push(this.create_operation(2, suffix))
+      this.inputStack.push(this.createOperation(2, suffix))
     } else {
       console.log(`Tried to delete a non integer value: ${k}`)
       process.exit(1)
@@ -35,7 +33,9 @@ class TextEditor {
 
   print (k) {
     if (Number.isInteger(k)) {
-      console.log(this.string.slice(0, k))
+      const output = this.string.slice(0, k)
+      console.log(output)
+      return output
     } else {
       console.log(`Tried to print a non integer amount of chars: ${k}`)
       process.exit(1)
@@ -43,16 +43,15 @@ class TextEditor {
   }
 
   undo () {
-    if (this.input_stack.length === 0) {
-      console.log('nothing to undo')
-      return
+    if (this.inputStack.length === 0) {
+      return ''
     }
-    const pair = this.input_stack.pop()
+    const pair = this.inputStack.pop()
     if (pair.operation === 1) {
       const wordLen = pair.text.length
-      this.delete(wordLen)
+      return this.delete(wordLen)
     } else if (pair.operation === 2) {
-      this.append(pair.text)
+      return this.append(pair.text)
     }
   }
 }
