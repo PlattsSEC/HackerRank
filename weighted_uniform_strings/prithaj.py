5d83058c1ee00a595b72953dca2418a3a8cc571b
Prithaj <prithajnath@gmail.com>
2017-06-23 06:42:19 +0000

    Why the fuck didn't I do this before

--- weighted_uniform_strings/prithaj.py
+++ weighted_uniform_strings/prithaj.py
@@ -1,10 +1,4 @@
 #!/bin/python
-
-import sys
-import string
-
-weights = {string.lowercase[i]:i+1 for i in xrange(len(string.lowercase))}
-
 def return_weights(a):
     my_weights, prev, count = set(), '', 1
     for i in a:
@@ -13,7 +7,7 @@ def return_weights(a):
             count = 1
         else:
             count +=1
-        my_weights.add(weights[i]*count)
+        my_weights.add((ord(i)-96)*count)
     return my_weights
             
 s = raw_input().strip()
@@ -21,7 +15,4 @@ n = input()
 check = return_weights(s)
 for _ in xrange(n):
     x = int(raw_input().strip())
-    if x in check:
-        print "Yes"
-    else:
-        print "No"
+    print("Yes" if x in check else "No")
