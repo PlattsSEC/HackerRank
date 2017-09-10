3b2b72436459dcf58ab07466d5a2ed0425b17962
Kevin Boyette <kevinxboyette@gmail.com>
2017-09-10 13:03:24 -0400

    Add Diagonal Difference HackerRank Problem
    * https://www.hackerrank.com/challenges/diagonal-difference

---
+++ algorithms/diagonal_difference/kevin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from typing import List
+
+def get_matrix_row_from_input() -> List[int]:
+    return [int(index) for index in input().strip().split(' ')]
+
+
+n = int(input().strip())
+primary_diag_sum = 0
+secondary_diag_sum = 0
+for row_count in range(n):
+    row = get_matrix_row_from_input()
+    primary_diag_sum += row[row_count]
+    secondary_diag_sum += row[-1 - row_count]
+
+print(abs(primary_diag_sum - secondary_diag_sum))
