c04324edd04503e514bb617a782fcd029bb07d68
Prithaj Nath <prithajnath@gmail.com>
2017-10-31 22:29:45 -0400

    Prithaj sherlock valid string (#26)
    * Add py solution to sherlock valid string :)
    
    * Clean namespace

---
+++ algorithms/strings/sherlock_and_the_valid_string/prithaj.py
@@ -0,0 +1,20 @@
+#!/bin/python
+from collections import Counter
+import sys
+
+def is_valid(s):
+    if len(set(Counter(s).values())) == 1:
+        return True
+    else:
+        return False
+
+s = raw_input().strip()
+if is_valid(s):
+    print "YES"
+else:
+    for i in set(s):
+        if is_valid(s.replace(i,"",1)):
+            print "YES"
+            break
+    else:
+        print "NO"
