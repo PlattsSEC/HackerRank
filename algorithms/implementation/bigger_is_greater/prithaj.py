fd76f724f858b76bcfb5832dd6f1448e2871b481
Prithaj <prithajnath@gmail.com>
2017-11-16 07:05:30 +0000

    Reduce no of lines

--- algorithms/implementation/bigger_is_greater/prithaj.py
+++ algorithms/implementation/bigger_is_greater/prithaj.py
@@ -2,9 +2,7 @@
 
 T = input()
 for _ in range(T):
-    x = list(raw_input())
-    suffix = [] 
-    
+    x, suffix = list(raw_input()), []
     for j in x[::-1]: 
         if suffix:
             if suffix[-1] <= j:
@@ -13,19 +11,14 @@ for _ in range(T):
                 break
         else:
             suffix.append(j)
-    
-    suffix = suffix[::-1]
-    prefix = x[:len(x)-len(suffix)]
-
+    suffix, prefix = suffix[::-1], x[:len(x)-len(suffix)]
     if prefix:
         for k in range(len(suffix)):
             if suffix[-k-1] > prefix[-1]:
                 prefix_succ = -k-1
                 break
-
         prefix[-1], suffix[prefix_succ] = suffix[prefix_succ], prefix[-1]
         suffix = suffix[::-1]
         print "".join(prefix+suffix)
-        
     else:
         print "no answer"
